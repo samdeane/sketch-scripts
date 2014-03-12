99df7b870d557aed88983ba2373dcc05d6a48b81
Sam Deane <sam@elegantchaos.com>
2014-03-12 00:38:02 +0000

    log window experiments

--- elegantchaos.js
+++ elegantchaos.js
@@ -1,4 +1,4 @@
-var com = {};
+var com = com || {};
 
 com.elegantchaos = {
 	export: function(document, kind){
@@ -10,12 +10,51 @@ com.elegantchaos = {
 		var slice = [[[document currentPage] sliceContainer] layerAtIndex:0];
 		[document saveArtboardOrSlice:slice toFile:export_path];
 	},
-	menuCommand: function(commandToPerform) {
-		log('doodah')
+
+	sendAction: function(commandToPerform) {
 		try {
 			[NSApp sendAction:commandToPerform to:nil from:doc]
 		} catch(e) {
 			log(e)
 		}
+	},
+
+	selection: function() {
+		if (selection == null) {
+			selection = [[NSArray alloc] init]
+		}
+
+		return selection;
+	},
+
+	makeLogWindow: function() {
+		var frame = NSMakeRect(0,0,512,256);
+		var window = [[NSWindow alloc] initWithContentRect:frame styleMask:1+2+8 backing:2 defer:true];
+		window.title = "Console";
+		var textField = [[NSTextField alloc] initWithFrame:[[window contentView] bounds]];
+		[textField setStringValue:"Testing"];
+		[textField setBordered:NO];
+		[textField setEditable:YES];
+		[textField setDrawsBackground:YES];
+		[textField setAlignment:NSCenterTextAlignment];
+		[[window contentView] addSubview:textField];
+
+		[window setReleasedWhenClosed:NO];
+		[window makeKeyAndOrderFront:nil];
+
+		log("made new log window");
+
+		return window;
+	},
+
+	log: function(message) {
+		if (com.elegantchaos.logWindow == null) {
+			com.elegantchaos.logWindow = com.elegantchaos.makeLogWindow();
+		}
+
+		textField = [[com.elegantchaos.logWindow contentView] subviews][0];
+		text = [textField stringValue];
+		text = text + "\n" + message;
+		[textField setStringValue:text];
 	}
 };
